4db8894c8b119fa2370d1922530cee0168fb6058
danielszakacs <daniel.sokach@gmail.com>
2019-02-12 13:06:03 +0100

    layers created.

---
+++ src/main/java/com/uzhorod/danielszakacs/uzhorodtouristguide/UzhorodtouristguideApplication.java
@@ -0,0 +1,14 @@
+package com.uzhorod.danielszakacs.uzhorodtouristguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzhorodtouristguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzhorodtouristguideApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/uzhorod/danielszakacs/uzhorodtouristguide/UzhorodtouristguideApplicationTests.java
@@ -0,0 +1,17 @@
+package com.uzhorod.danielszakacs.uzhorodtouristguide;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UzhorodtouristguideApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
